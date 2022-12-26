6734da01ebcc01a39081d7874b58ad4eabf3e9ba
Ayan Saha <49653970+Rooghz@users.noreply.github.com>
2022-12-27 02:03:54 +0530

    Create 3. Vowels Finder.js

---
+++ Basics of JS/3. Vowels Finder.js
@@ -0,0 +1,16 @@
+const input = "Hello";
+
+const vowels = "aeiou";
+
+for(let i=0; i<input.length; i++){
+    if(vowels.includes(input[i])){
+        console.log(input[i]);
+    }
+}
+
+/*
+
+e
+o
+
+*/
