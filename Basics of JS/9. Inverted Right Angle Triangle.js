4710ede9521e389ea7b17ad1d7a5f83177ff2b6b
Ayan Saha <49653970+Rooghz@users.noreply.github.com>
2022-12-28 00:30:01 +0530

    Create 9. Inverted Right Angle Triangle.js

---
+++ Basics of JS/9. Inverted Right Angle Triangle.js
@@ -0,0 +1,21 @@
+invTriangleWall(num) {
+        // code here
+        let star = "* ";
+        for(let i=num; i>=1; i--){
+            console.log(star.repeat(i));
+        }
+    }  
+};
+
+
+/*
+
+For Input: 
+4
+Your Output: 
+* * * *
+* * *
+* *
+*
+
+*/
