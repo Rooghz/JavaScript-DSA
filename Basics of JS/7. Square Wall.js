db5317a84da7ccabf63a2677a75ae1ef196d58e6
Ayan Saha <49653970+Rooghz@users.noreply.github.com>
2022-12-28 00:02:50 +0530

    Create 7. Square Wall.js

---
+++ Basics of JS/7. Square Wall.js
@@ -0,0 +1,19 @@
+squareWall(num) {
+        // code here
+        for(let i=0; i<num; i++){
+            for(let j=0; j<num; j++){
+                process.stdout.write("* ");
+            }
+            console.log("");
+        }
+  
+  
+/*
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+*/
