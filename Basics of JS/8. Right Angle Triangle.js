31b5f1e55ba753e3863ac0750faaa3399f935337
Ayan Saha <49653970+Rooghz@users.noreply.github.com>
2022-12-28 00:22:22 +0530

    Create 8. Right Angle Triangle.js

---
+++ Basics of JS/8. Right Angle Triangle.js
@@ -0,0 +1,20 @@
+triangleWall(num) {
+        // code here
+        let star = "* ";
+        for(let i=1; i<=num; i++){
+            console.log(star.repeat(i));
+        }
+}
+
+
+/*
+
+For Input: 
+4
+Your Output: 
+*
+* *
+* * *
+* * * *
+
+*/
